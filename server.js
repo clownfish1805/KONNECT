eda8564890cda8339089b4cffea01e765da548dc
akkshara <akkshara.ss2021cce@sece.ac.in>
2024-05-11 19:00:04 +0530

    proper working

--- server.js
+++ server.js
@@ -8,18 +8,17 @@ const io = require('socket.io')(server);
 
 app.use(express.static(path.join(__dirname + "/public")));
 
-
-io.on("connection",function(socket){
-    socket.on("newuser",function(username){
-        socket.broadcast.emit("update",username+ "joined the convo");
+io.on("connection", function(socket) {
+    socket.on("newuser", function(username) {
+        socket.broadcast.emit("update", username + " joined the convo");
     });
-    socket.on("exituser",function(username){
-        socket.broadcast.emit("update",username+ "left the convo");
-    })
-    socket.on("chat",function(message){
-        socket.broadcast.emit("chat",message);
-    })
-})
+    socket.on("exituser", function(username) {
+        socket.broadcast.emit("update", username + " left the convo");
+    });
+    socket.on("chat", function(message) {
+        socket.broadcast.emit("chat", message);
+    });
+});
 
 // server.listen(5000);
 
